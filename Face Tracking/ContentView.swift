fe01e2427c0c128c294223bed00acf9f3c2cb30e
Ravi <26054135+pansuriyaravi@users.noreply.github.com>
2024-05-15 13:36:25 +0530

    Initial Commit

---
+++ Face Tracking/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Face Tracking
+//
+//  Created on 15/05/24.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Face Tracking/Face_TrackingApp.swift
@@ -0,0 +1,18 @@
+//
+//  Face_TrackingApp.swift
+//  Face Tracking
+//
+//  Created on 15/05/24.
+//  
+//
+
+import SwiftUI
+
+@main
+struct Face_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
